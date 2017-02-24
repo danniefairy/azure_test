eea15f10fde1fa8895c79f56545257c7f04cc722
Dannie <dannienctu@gmail.com>
2017-02-24 21:53:26 +0800

    db test

--- test.php
+++ test.php
@@ -1,3 +1,25 @@
 <?php
-	echo 123;
+	$dbserver='ja-cdbr-azure-east-a.cloudapp.net';
+	$dbuser='b65d289d7448d9';//Mifriend
+	$dbpassowrd='92fd4ee3';//Mifriend
+
+	$connect=mysqli_connect($dbserver,$dbuser,$dbpassowrd);
+	if(mysqli_connect_errno($connect))
+		die("fail to connect the db");
+	else
+		//echo 'connect successfully';
+
+	//echo "<br>";
+
+	//選擇資料庫
+	if(!mysqli_select_db($connect,'connect_test'))//Mifriend
+		die('can not use db');
+	else
+		//echo 'mifriend db can be used';
+
+	//echo "<br>";
+
+	//設定中文傳輸
+	$set_chinese='SET NAMES utf8';
+	mysqli_query($connect,$set_chinese);		
 ?>
