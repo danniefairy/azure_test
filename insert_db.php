2dcfb36f2fb6487d78ee7254456ce57d891d6ed4
Dannie <dannienctu@gmail.com>
2017-02-25 14:04:45 +0800

    change phone type to text

--- insert_db.php
+++ insert_db.php
@@ -18,7 +18,7 @@
 		}
 	}
 
-	$update="UPDATE `questionnaire` SET `name`=$name,`phone`=$phone,`one`=\"$insert[2]\",`two`=\"$insert[3]\",`three`=\"$insert[4]\",`four`=\"$insert[5]\",`five`=\"$insert[6]\",`six`=\"$insert[7]\",`seven`=\"$insert[8]\" WHERE `id`=$id";
+	$update="UPDATE `questionnaire` SET `name`=$name,`phone`=\"$phone\",`one`=\"$insert[2]\",`two`=\"$insert[3]\",`three`=\"$insert[4]\",`four`=\"$insert[5]\",`five`=\"$insert[6]\",`six`=\"$insert[7]\",`seven`=\"$insert[8]\" WHERE `id`=$id";
 	mysqli_query($connect,$update);
 	echo "<script>
 				var url=\"finish_page.php?name=$insert[0]&phone=$insert[1]\";
